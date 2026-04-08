dfa9276a442217b2c851b8da34e6f4573d0e6a2e
CodyJReed <cody2j2reed@gmail.com>
2026-04-08 11:11:21 -0400

    add image and supply prop content to render; additonal content render to match rating and review_count

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -4,11 +4,25 @@ import { View, Image, StyleSheet, Text } from "react-native";
 const ResultsDetail = ({item}) => {
   return (
     <View>
-      <Text>{item.name}</Text>
+      <Image source={{
+        uri: item.image_url
+      }} style={styles.imageStyle}/>
+      <Text style={styles.nameStyle}>{item.name}</Text>
+      <Text>{item.rating} Starts, {item.review_count} Reviews</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  nameStyle: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
 
 export default ResultsDetail
